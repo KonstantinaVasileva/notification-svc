61a801e3f831a6e278a7a81b225e0d1da09fe8bb
Konstantina Vasileva <konstantina@mail.bg>
2025-03-21 22:19:16 +0200

    Initial commit

---
+++ src/test/java/bg/softuni/notification_svc/NotificationSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.notification_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
