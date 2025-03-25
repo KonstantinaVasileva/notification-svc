2332e5d963391ac313832e47d961102586516d47
Konstantina Vasileva <konstantina@mail.bg>
2025-03-25 10:52:46 +0200

    exception handler

--- src/main/java/bg/softuni/notification_svc/model/NotificationType.java
+++ src/main/java/bg/softuni/notification_svc/model/NotificationType.java
@@ -1,8 +1,5 @@
 package bg.softuni.notification_svc.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-//@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum NotificationType {
     INFO, WARNING, WELCOME
 }
